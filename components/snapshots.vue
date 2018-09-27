7c216cbfee83ffaa2096b8d32d3a8b5c24ff66d1
Gerald <gera2ld@163.com>
2018-09-27 21:02:19 +0800

    feat: support dragging on snapshots

--- components/snapshots.vue
+++ components/snapshots.vue
@@ -3,7 +3,14 @@
     <h3>My Snapshots</h3>
     <div class="empty" v-if="!snapshots.length"><div class="empty-title">None</div></div>
     <ul class="menu snapshots" v-else>
-      <li class="menu-item" v-for="(item, index) in snapshots">
+      <li
+        class="menu-item"
+        v-for="(item, index) in snapshots"
+        :class="{'snapshot-placeholder': dragging && dragging.index === index}"
+        draggable="true"
+        @dragstart="onDragStart($event, index)"
+        @dragend="onDragEnd"
+        @dragover="onDragOver($event, index)">
         <button class="btn btn-clear float-right mt-2" @click="onRemove(index)"></button>
         <a href="#" :class="{active: active === item}" v-text="item.name" @click.prevent="onPick(item, index)"></a>
       </li>
@@ -22,6 +29,7 @@ export default {
   ],
   data() {
     return {
+      dragging: null,
       snapshots: [],
     };
   },
@@ -47,6 +55,39 @@ export default {
       this.dump();
       return item;
     },
+    onDragStart(e, index) {
+      const {
+        clientX,
+        clientY,
+        offsetX,
+        offsetY,
+        target: { offsetWidth, offsetHeight },
+      } = e;
+      const style = {
+        width: `${offsetWidth}px`,
+        height: `${offsetHeight}px`,
+        top: `${clientY - offsetY}px`,
+        left: `${clientX - offsetX}px`,
+      };
+      this.dragging = {
+        index,
+        style,
+        offsetX,
+        offsetY,
+        item: this.snapshots[index],
+      };
+    },
+    onDragOver(e, index) {
+      const { dragging } = this;
+      if (dragging.index === index) return;
+      const item = this.snapshots[index];
+      Vue.set(this.snapshots, dragging.index, item);
+      Vue.set(this.snapshots, dragging.index = index, dragging.item);
+      this.storage.dump(this.snapshots);
+    },
+    onDragEnd() {
+      this.dragging = null;
+    },
   },
   mounted() {
     this.storage = getStorage(this.storageKey);
@@ -64,6 +105,12 @@ export default {
   }
   .menu-item > a:not(.active):focus {
     background: none;
+  }
+}
+
+.snapshot {
+  &-placeholder {
+    opacity: 0.2;
   }
 }
 </style>
