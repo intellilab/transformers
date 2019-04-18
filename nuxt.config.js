5d48c82dea39685305c776ac57fa1571976aaf7a
Gerald <gera2ld@163.com>
2019-04-18 14:03:19 +0800

    fix: update icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,6 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ],
   },
   loading: { color: '#3B8070' },
   css: [
