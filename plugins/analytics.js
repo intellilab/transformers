17d652ee11d72dc08f185581018a3e874f06bbff
Gerald <gera2ld@163.com>
2019-05-06 17:43:32 +0800

    feat: add analytics script

---
+++ plugins/analytics.js
@@ -0,0 +1,6 @@
+export default ({ app }) => {
+  window._hmt = window._hmt || [];
+  const hm = document.createElement('script');
+  hm.src = 'https://hm.baidu.com/hm.js?9802249e266d29eadb5bd2c0f51ce59c';
+  document.head.append(hm);
+};
