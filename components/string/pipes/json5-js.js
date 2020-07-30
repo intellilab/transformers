98b4865028cb4434096c34e5140a21d37da04e0f
Gerald <gera2ld@163.com>
2020-07-30 09:55:06 +0800

    fix: format json with @gera2ld/format-json

--- components/string/pipes/json5-js.js
+++ components/string/pipes/json5-js.js
@@ -1,148 +1,34 @@
-import JSON5 from 'json5';
-
-const MULTILINE = 'MULTILINE';
-const SINGLELINE = 'SINGLELINE';
-const KEY = 'KEY';
-const COMMA = { type: 'comma', value: ',' };
-const BR = { type: 'br', value: '\n' };
-
-const charMapBase = {
-  '\\': '\\\\',
-  '\r': '\\r',
-  '\t': '\\t',
-};
-const charMapQuote = {
-  ...charMapBase,
-  '\'': '\\\'',
-  '\n': '\\n',
-};
-const charMapTemplate = {
-  ...charMapBase,
-  '`': '\\`',
-};
-function quoteString(str, { quote, template }) {
-  if (template && /\n/.test(str)) {
-    const quoted = str.replace(/[\\`\r\t]/g, m => charMapTemplate[m]);
-    return `\`${quoted}\``;
-  }
-  if (quote || /\W/.test(str)) {
-    const quoted = str.replace(/[\\'\r\n\t]/g, m => charMapQuote[m]);
-    return `'${quoted}'`;
-  }
-  return str;
-}
-
-function getSpace(level, indent) {
-  return { type: 'space', value: ' '.repeat(indent * level) };
-}
-
-function render(data, options, level = 0) {
-  if (Array.isArray(data)) {
-    const arr = [];
-    const ret = {
-      type: MULTILINE,
-      separator: [COMMA],
-      data: arr,
-    };
-    arr.push({ value: '[' });
-    if (data.length) {
-      const rendered = data.map(item => render(item, options, level + 1));
-      arr.push(
-        BR,
-        getSpace(level + 1, options.indent),
-        ...join(rendered, options, level + 1),
-        BR,
-        getSpace(level, options.indent),
-      );
-    } else {
-      ret.type = SINGLELINE;
-    }
-    arr.push({ value: ']' });
-    return ret;
-  }
-  if (data === null) {
-    return {
-      type: SINGLELINE,
-      separator: [COMMA],
-      data: [{ value: data, type: 'null' }],
-    };
-  }
-  if (typeof data === 'object') {
-    const arr = [];
-    const ret = {
-      type: MULTILINE,
-      separator: [COMMA],
-      data: arr,
-    };
-    arr.push({ value: '{' });
-    const rendered = Object.keys(data).flatMap(key => [
-      {
-        type: KEY,
-        data: [{ value: quoteString(key, options), type: 'key' }],
-        separator: [{ value: ':' }],
-      },
-      render(data[key], options, level + 1),
-    ]);
-    if (rendered.length) {
-      arr.push(
-        BR,
-        getSpace(level + 1, options.indent),
-        ...join(rendered, options, level + 1),
-        BR,
-        getSpace(level, options.indent),
-      );
-    } else {
-      ret.type = SINGLELINE;
-    }
-    arr.push({ value: '}' });
-    return ret;
-  }
-  return {
-    type: SINGLELINE,
-    separator: [COMMA],
-    data: [{ value: typeof data === 'string' ? quoteString(data, { ...options, quote: 1 }) : data }],
-  };
-}
-
-function join(rendered, options, level) {
-  const arr = [];
-  for (let i = 0; i < rendered.length; i += 1) {
-    const item = rendered[i];
-    const next = rendered[i + 1];
-    if (item.data) arr.push(...item.data);
-    // trailing separators
-    if (item.separator && (next || options.trailing)) {
-      arr.push(...item.separator);
-    }
-    if (next) {
-      if (item.type === KEY) {
-        arr.push({ value: ' ' });
-      } else {
-        arr.push(BR, getSpace(level, options.indent));
-      }
-    }
-  }
-  return arr;
-}
+import { format } from '@gera2ld/format-json';
 
 export function handle(input, options) {
-  const obj = JSON5.parse(input);
-  const rendered = render(obj, options);
-  return rendered.data.map(({ value }) => `${value}`).join('');
+  return format(input, {
+    ...options,
+    quoteAsNeeded: !!+options.quoteAsNeeded,
+  });
 }
 
 export const meta = {
   name: 'JSON5 to JavaScript',
   options: [
     {
-      name: 'quote',
-      description: 'Quote',
+      name: 'quoteAsNeeded',
+      description: 'Add quotes',
       type: 'radio',
       choices: [
-        { value: 0, label: 'as needed' },
-        { value: 1, label: 'always' },
+        { value: 1, label: 'as needed' },
+        { value: 0, label: 'always' },
       ],
-      default: 0,
+      default: 1,
+    },
+    {
+      name: 'quote',
+      description: 'Preferred quote character',
+      type: 'radio',
+      choices: [
+        { value: '\'', label: '\'' },
+        { value: '"', label: '"' },
+      ],
+      default: '\'',
     },
     {
       name: 'indent',
