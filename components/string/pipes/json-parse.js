673b67185ced2968e94c200b25dba36c748d8da7
Gerald <gera2ld@163.com>
2020-08-21 22:41:55 +0800

    feat: add json-parse pipe

---
+++ components/string/pipes/json-parse.js
@@ -0,0 +1,12 @@
+import JSON5 from 'json5';
+
+export function handle(input, options) {
+  let obj = JSON5.parse(input);
+  if (typeof obj !== 'string') throw new Error('Cannot parse JSON');
+  return obj;
+}
+
+export const meta = {
+  name: 'JSON parse',
+  options: [],
+};
