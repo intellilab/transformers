b231b0f18c3a4331aee45af21b0c92ca41767009
Gerald <code@gera2ld.space>
2026-08-16 20:34:42 +0900

    feat: redesign string pipes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname),
+      '@': path.resolve(__dirname),
+    },
+  },
   test: {
     include: ['components/**/*.test.ts'],
   },
